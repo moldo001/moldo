858e21ec247fc2d99afcf0dc35af63cd18832c6b
moldo001 <misisipi405@gmail.com>
2022-09-18 20:37:56 +0300

    mesaj

---
+++ src/main/java/org/example/T3_ex1.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class T3_ex1 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
